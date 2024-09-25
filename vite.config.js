b2b6a618b30e9e00cbb012bb33f6a92491f98070
Will <wtdavis@wesleyan.edu>
2024-09-25 17:33:29 -0400

    redo

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,5 @@ import { defineConfig } from "vite";
 import { react } from "@vitejs/plugin-react"
 
 export default defineConfig({
-    def:
-    def:
-    def:
-    def"
-    def:
-    herp
-    derp
-    derpus
-    mcderp
-    mcflerp
-    mcflerpsus
-    mcderflus
-    DEFL
+  
 })
